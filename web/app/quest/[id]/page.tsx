710268b810d10ba9121d99e855179ad2bea03fa7
MrRishabThapa <rishabthapa7777@gmail.com>
2026-09-13 14:10:03 +0545

    Remove old quest route location

--- web/app/quest/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { QuestScreen } from "@/components/screens/QuestScreen";
-
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return <QuestScreen id={id} />;
-}
-

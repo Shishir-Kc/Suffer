c96873c36ca9614c26eee5f28cb2b38ab1406669
MrRishabThapa <rishabthapa7777@gmail.com>
2026-09-24 14:15:08 +0545

    chore: update app/(with-nav)/home/page.tsx

--- web/app/(with-nav)/home/page.tsx
+++ web/app/(with-nav)/home/page.tsx
@@ -1,3 +1,5 @@
 import { HomeScreen } from "@/components/screens/HomeScreen";
 
-export default function Page() { return <HomeScreen />; }
+export default function Page() {
+  return <HomeScreen />;
+}

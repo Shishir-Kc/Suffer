78b7b6bbbe3fd7706ef803769a91e7fb892f631c
MrRishabThapa <rishabthapa7777@gmail.com>
2026-09-24 14:15:08 +0545

    chore: update app/(with-nav)/layout.tsx

--- web/app/(with-nav)/layout.tsx
+++ web/app/(with-nav)/layout.tsx
@@ -2,5 +2,10 @@ import type { ReactNode } from "react";
 import { BottomNav } from "@/components/ui/BottomNav";
 
 export default function WithNavLayout({ children }: { children: ReactNode }) {
-  return <>{children}<BottomNav /></>;
+  return (
+    <>
+      {children}
+      <BottomNav />
+    </>
+  );
 }

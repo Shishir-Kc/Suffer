98636f2527350d28a3e33826c008bbaf5da1b308
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-09-24 15:02:55 +0545

    Define Worker environment types

--- Backend/src/types/env.ts
+++ Backend/src/types/env.ts
@@ -1,3 +1,9 @@
-export type Env = {
-  DB:D1Database;
-};
+export type Env = CloudflareBindings & {
+  JWT_SECRET: string
+  B2_BUCKET_NAME: string
+  B2_APPLICATION_KEY_ID: string
+  B2_APPLICATION_KEY: string
+  FACE_SERVICE_URL: string
+  FACE_SERVICE_TOKEN: string
+  FACE_CALLBACK_TOKEN: string
+}

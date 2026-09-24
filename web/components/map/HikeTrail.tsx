9e2e047de501310c94b062f24fa15831e5ac46f7
MrRishabThapa <rishabthapa7777@gmail.com>
2026-09-24 14:15:08 +0545

    chore: update components/map/HikeTrail.tsx

--- web/components/map/HikeTrail.tsx
+++ web/components/map/HikeTrail.tsx
@@ -1,2 +1,5 @@
-export function HikeTrail() { return <span className="hike-trail" aria-label="Bhedetar to Namje hike trail" />; }
-
+export function HikeTrail() {
+  return (
+    <span className="hike-trail" aria-label="Bhedetar to Namje hike trail" />
+  );
+}

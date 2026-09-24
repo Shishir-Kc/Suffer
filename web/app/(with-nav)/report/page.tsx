186ebb0e3cb7115301d854e9d12bc1275b8efa7a
MrRishabThapa <rishabthapa7777@gmail.com>
2026-09-24 14:15:08 +0545

    chore: update app/(with-nav)/report/page.tsx

--- web/app/(with-nav)/report/page.tsx
+++ web/app/(with-nav)/report/page.tsx
@@ -1,3 +1,5 @@
 import { ReportScreen } from "@/components/screens/ReportScreen";
 
-export default function Page() { return <ReportScreen />; }
+export default function Page() {
+  return <ReportScreen />;
+}

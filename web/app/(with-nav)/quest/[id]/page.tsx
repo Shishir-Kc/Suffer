1f09b5d1738242b0499e1df81be1f99c2194a802
MrRishabThapa <rishabthapa7777@gmail.com>
2026-09-24 14:15:08 +0545

    chore: update app/(with-nav)/quest/[id]/page.tsx

--- web/app/(with-nav)/quest/[id]/page.tsx
+++ web/app/(with-nav)/quest/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { QuestScreen } from "@/components/screens/QuestScreen";
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   return <QuestScreen id={id} />;
 }
